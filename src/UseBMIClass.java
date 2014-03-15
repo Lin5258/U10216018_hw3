d2dfbfefe2135321e2bc44dee78d2ed9f75df7ce
Lin5258 <gogolin1688@yahoo.com.tw>
2014-03-15 18:37:23 +0800

    Change to UI version

--- src/UseBMIClass.java
+++ src/UseBMIClass.java
@@ -1,13 +1,31 @@
 
-public class UseBMIClass {
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+
+public class UseBMIClass extends JFrame {
+	
+	public UseBMIClass() {
+		// Set FlowLayout, aligned left with horizontal gap 10
+		// and vertical gap 20 between components
+		setLayout(new FlowLayout(FlowLayout.LEFT, 10, 20));
+		
+		BMI bmi1 = new BMI("Kim Yang", 18, 145, 70);
+		add(new JLabel("The BMI for " + bmi1.getName() + " is " + bmi1.getBMI() + " " + bmi1.getStatus()));
+		
+		BMI bmi2 = new BMI("Susan King", 215, 70);
+		add(new JLabel("The BMI for " + bmi2.getName() + " is " + bmi2.getBMI() + " " + bmi2.getStatus()));
+	}
 
 	public static void main(String[] args) {
 		
-		BMI bmi1 = new BMI("Kim Yang", 18, 145, 70);
-		System.out.println("The BMI for " + bmi1.getName() + " is " + bmi1.getBMI() + " " + bmi1.getStatus());
+		UseBMIClass frame = new UseBMIClass();
 		
-		BMI bmi2 = new BMI("Susan King", 215, 70);
-		System.out.println("The BMI for " + bmi2.getName() + " is " + bmi2.getBMI() + " " + bmi2.getStatus());
+		frame.setTitle("BMI");
+		frame.setSize(400, 200);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 
 	}
 
