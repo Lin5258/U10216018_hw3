0188ebab7693b210343105ec5eaf4df1d206ed85
Lin5258 <gogolin1688@yahoo.com.tw>
2014-03-15 22:08:15 +0800

    Add notaion

--- src/UseBMIClass.java
+++ src/UseBMIClass.java
@@ -10,22 +10,22 @@ public class UseBMIClass extends JFrame {
 		// and vertical gap 20 between components
 		setLayout(new FlowLayout(FlowLayout.LEFT, 10, 20));
 		
-		BMI bmi1 = new BMI("Kim Yang", 18, 145, 70);
-		add(new JLabel("The BMI for " + bmi1.getName() + " is " + bmi1.getBMI() + " " + bmi1.getStatus()));
+		BMI bmi1 = new BMI("Kim Yang", 18, 145, 70);	//Construct an object with name, age, height and weight.
+		add(new JLabel("The BMI for " + bmi1.getName() + " is " + bmi1.getBMI() + " " + bmi1.getStatus()));		//Add label
 		
-		BMI bmi2 = new BMI("Susan King", 215, 70);
-		add(new JLabel("The BMI for " + bmi2.getName() + " is " + bmi2.getBMI() + " " + bmi2.getStatus()));
+		BMI bmi2 = new BMI("Susan King", 215, 70);		//Construct an object with name, height and weight.
+		add(new JLabel("The BMI for " + bmi2.getName() + " is " + bmi2.getBMI() + " " + bmi2.getStatus()));		//Add label
 	}
 
 	public static void main(String[] args) {
 		
-		UseBMIClass frame = new UseBMIClass();
+		UseBMIClass frame = new UseBMIClass();		//Construct an instance of UseBMIClass.
 		
-		frame.setTitle("BMI");
-		frame.setSize(400, 200);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		frame.setTitle("BMI");		//Set frame title.
+		frame.setSize(400, 200);	//Set frame size.
+		frame.setLocationRelativeTo(null);		//Set frame location.
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		//Let frame can be close.
+		frame.setVisible(true);		//Let frame can be seem.
 
 	}
 
